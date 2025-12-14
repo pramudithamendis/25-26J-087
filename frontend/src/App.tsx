44302d5c4a77c903ad0cff56a469934129f14654
pramudithamendis <134496244+pramudithamendis@users.noreply.github.com>
2025-12-14 16:05:50 +0530

    Finding best project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import QuestionsCVUpload from "./Questions/QuestionsCVUpload";
+import QuestionsFindBestProject from "./Questions/QuestionsFindBestProject";
 
 function App() {
   return (
     <Routes>
       <Route path="/questions/upload" element={<QuestionsCVUpload />} />
+      <Route path="/questions/bestproject" element={<QuestionsFindBestProject />} />
     </Routes>
   );
 }
